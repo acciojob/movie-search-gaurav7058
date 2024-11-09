3fc0ef0fb2247fd2938a3edfb5cd1d4ba6500779
Create_first_repo <gauravchoundiye123@gmail.com>
2024-11-09 18:50:57 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,45 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState('');
+  function getApi(){
+    fetch(`https://www.omdbapi.com/?apikey=99eb9fd1&s=${query}`).then(res=>res.json()).then(json=>{
+      if(json.Response=="True"){
+        setMovies(json.Search);
+        console.log(json.Search)
+        setError("")
+      }
+      else{
+        setError("Invalid movie name. Please try again.");
+        setMovies([])
+      }
+    });
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <div className='container'>
+      <h1>Search Movies</h1>
+      <input type="text" value={query} onChange={(e)=>setQuery(e.target.value)}></input>
+      <button onClick={getApi}>Search</button>
+      {
+        movies.map((item,id)=>{
+          return(
+            <div className="" key={id}>
+                <h1>{item.Title} ({item.Year})</h1>
+                <img src={item.Poster} alt="" />
+            </div>
+          )
+        })
+      }
+      <div className="error">
+         <h1>{error}</h1>
+      </div>
+    </div>
     </div>
   )
 }
