bb2aeea99b43e42669e5d31f595725af8aeed120
Create_first_repo <gauravchoundiye123@gmail.com>
2024-11-09 19:03:36 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import './../styles/App.css';
 
@@ -6,50 +5,56 @@ const App = () => {
   const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
   const [error, setError] = useState('');
-  function getApi(){
-    fetch(`https://www.omdbapi.com/?apikey=99eb9fd1&s=${query}`).then(res=>res.json()).then(json=>{
-      if(json.Response=="True"){
-        setMovies(json.Search);
-        console.log(json.Search)
-        setError("")
-      }
-      else{
-        setError("Invalid movie name. Please try again.");
-        setMovies([])
-      }
-    });
-  }
+
+  const getApi = () => {
+    fetch(`https://www.omdbapi.com/?apikey=99eb9fd1&s=${query}`)
+      .then(res => res.json())
+      .then(json => {
+        if (json.Response === "True") {
+          setMovies(json.Search);
+          setError('');
+        } else {
+          setError("Invalid movie name. Please try again.");
+          setMovies([]);
+        }
+      });
+  };
 
   return (
     <div>
-        <div className='container'>
-      <form action="" onClick={(e)=>e.preventDefault()}>
-      <h1>Search Movies</h1>
-      <input type="text" value={query} onChange={(e)=>setQuery(e.target.value)}></input>
-      <button onClick={getApi}>Search</button>
-      </form>
-      {
-        movies.map((item,id)=>{
-          return(
-            <div className="" key={id}>
-                <ul>
-                  <li>
-                  <h1>{item.Title} ({item.Year})</h1>
-                  </li>
-                  <li>
-                  <img src={item.Poster} alt="" />
-                  </li>
-                </ul>
-            </div>
-          )
-        })
-      }
-      <div className="error">
-         <h1>{error}</h1>
+      <div className='container'>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <h1>Search Movies</h1>
+          <input
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Enter movie title..."
+          />
+          <button onClick={getApi}>Search</button>
+        </form>
+
+        {movies.slice(0, 10).map((item, id) => (
+          <div className="movie" key={id}>
+            <ul>
+              <li>
+                <h1>{item.Title} ({item.Year})</h1>
+              </li>
+              <li>
+                <img src={item.Poster} alt={item.Title} />
+              </li>
+            </ul>
+          </div>
+        ))}
+
+        {error && (
+          <div className="error">
+            <h1>{error}</h1>
+          </div>
+        )}
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
